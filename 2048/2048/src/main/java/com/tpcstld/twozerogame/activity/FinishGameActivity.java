cc368def45b570e1fbabb123e9e313e7516890d3
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2021-03-31 17:04:01 +0100

    Enabled view binding.

--- 2048/2048/src/main/java/com/tpcstld/twozerogame/activity/FinishGameActivity.java
+++ 2048/2048/src/main/java/com/tpcstld/twozerogame/activity/FinishGameActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.tpcstld.twozerogame.R;
+import com.tpcstld.twozerogame.databinding.ActivityFinishGameBinding;
 
 public class FinishGameActivity extends AppCompatActivity {
 
+  private ActivityFinishGameBinding binding;
+
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_finish_game);
+    binding = ActivityFinishGameBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
   }
 }
