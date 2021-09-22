5fd5f20914a5048a559d56701f3afecb91b70aea
Trinkes <fabiojscosta1583@gmail.com>
2021-09-22 18:06:57 +0100

    [ESKL-81] - remove set score retries, keeping only the last one, when the player status is completed show an error screen when an error occurs whole submitting the last score ignore set score errors while player status is PLAYING

--- 2048/2048/src/main/java/com/appcoins/eskills2048/vm/FinishGameActivityViewModel.java
+++ 2048/2048/src/main/java/com/appcoins/eskills2048/vm/FinishGameActivityViewModel.java
@@ -7,6 +7,7 @@ import com.appcoins.eskills2048.model.User;
 import com.appcoins.eskills2048.model.UserStatus;
 import com.appcoins.eskills2048.usecase.GetRoomUseCase;
 
+import com.appcoins.eskills2048.usecase.SetFinalScoreUseCase;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Single;
@@ -14,17 +15,31 @@ import io.reactivex.Single;
 public class FinishGameActivityViewModel {
 
   private final GetRoomUseCase getRoomUseCase;
+  private final SetFinalScoreUseCase setFinalScore;
   private final String session;
   private final String walletAddress;
+  private final long userScore;
 
-  public FinishGameActivityViewModel(GetRoomUseCase getRoomUseCase, String session, String walletAddress) {
+  public FinishGameActivityViewModel(GetRoomUseCase getRoomUseCase, SetFinalScoreUseCase setFinalScore,
+      String session, String walletAddress, long userScore) {
     this.getRoomUseCase = getRoomUseCase;
+    this.setFinalScore = setFinalScore;
     this.session = session;
     this.walletAddress = walletAddress;
+    this.userScore = userScore;
   }
 
   public Single<RoomResult> getRoomResult() {
     return getRoomUseCase.getRoom(session)
+        .flatMap(roomResponse -> {
+          if (roomResponse.getCurrentUser()
+              .getStatus() != UserStatus.PLAYING) {
+            return Single.just(roomResponse);
+          }
+          return setFinalScore.setFinalScore(session, userScore)
+              .doOnError(throwable -> throwable.printStackTrace())
+              .onErrorReturnItem(roomResponse);
+        })
         .toObservable()
         .repeatWhen(objectFlowable -> objectFlowable.delay(3, TimeUnit.SECONDS))
         .skipWhile(this::isInProgress)
@@ -34,14 +49,14 @@ public class FinishGameActivityViewModel {
   }
 
   public boolean isWinner(RoomResult roomResult) {
-    return roomResult.getWinner().getWalletAddress()
+    return roomResult.getWinner()
+        .getWalletAddress()
         .equalsIgnoreCase(walletAddress);
   }
 
-
   private boolean isInProgress(RoomResponse roomResponse) {
     boolean completed = roomResponse.getStatus() == RoomStatus.COMPLETED;
-    for (User user: roomResponse.getUsers()) {
+    for (User user : roomResponse.getUsers()) {
       if (user.getStatus() == UserStatus.PLAYING && completed) {
         throw new IllegalStateException("Match Completed but some players are still playing!");
       }
